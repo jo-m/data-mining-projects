eac2c25ace839b53e0a852fda63eaf609e126ca1
Jonathan Müller <j.mueller@apoveda.ch>
2015-10-26 11:54:38 +0100

    first try, but something is still very wrong

--- project2/mapper.py
+++ project2/mapper.py
@@ -3,20 +3,35 @@
 
 import sys
 import numpy as np
+import itertools
+from sklearn.svm import LinearSVC
 
-DIMENSION = 400  # Dimension of the original data.
-CLASSES = (-1, +1)   # The classes that we are trying to predict.
+BATCH_SIZE = 5000
 
 def transform(x_original):
     return x_original
 
-def main():
+def lines():
     for line in sys.stdin:
         line = line.strip()
         (label, x_string) = line.split(" ", 1)
         label = int(label)
         x_original = np.fromstring(x_string, sep=' ')
-        x = transform(x_original)  # Use our features.
+        yield label, transform(x_original)
+
+def main():
+    input = lines()
+    while True:
+        m = list(itertools.islice(input, 0, BATCH_SIZE))
+        if len(m) == 0:
+            break
+        X = np.vstack([x[1] for x in m])
+        Y = np.array([x[0] for x in m]).T
+
+        clf = LinearSVC(dual=False, fit_intercept=False)
+        clf.fit(X, Y)
+        np.savetxt(sys.stdout, clf.coef_)
 
 if __name__ == '__main__':
+    sys.stderr.write('Started a python process\n')
     main()
--- project2/reducer.py
+++ project2/reducer.py
@@ -1,9 +1,9 @@
 #!/local/anaconda/bin/python
 # IMPORTANT: leave the above line as is.
 
-import logging
 import sys
 import numpy as np
 
-for line in sys.stdin:
-    line = line.strip()
+coef = np.loadtxt(sys.stdin)
+coef = np.mean(coef, axis=0)
+np.savetxt(sys.stdout, coef, newline=" ")

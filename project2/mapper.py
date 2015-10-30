04eac841b59b0d14017191ecc84335662e896eaf
Marco Unternährer <marco.unter@gmail.com>
2015-10-30 10:21:59 +0100

    Set up for pycharm

--- project2/mapper.py
+++ project2/mapper.py
@@ -6,13 +6,15 @@ import numpy as np
 import itertools
 from sklearn.svm import LinearSVC
 
+pycharm_mode = False
+
 BATCH_SIZE = 5000
 
 def transform(x_original):
     return x_original
 
-def lines():
-    for line in sys.stdin:
+def lines(source):
+    for line in source:
         line = line.strip()
         (label, x_string) = line.split(" ", 1)
         label = int(label)
@@ -20,7 +22,24 @@ def lines():
         yield label, transform(x_original)
 
 def main():
-    input = lines()
+    if pycharm_mode:
+        import argparse
+        parser = argparse.ArgumentParser()
+        parser.add_argument("--input", help="The filename to be processed", required=True)
+        parser.add_argument("--output", help="The filename to be written to")
+        args = parser.parse_args()
+        if args.input:
+            input_f = open(args.input)
+            input = lines(input_f)
+
+        if args.output:
+            output = args.output
+        else:
+            output = sys.stdout
+    else:
+        input = lines(sys.stdin)
+        output = sys.stdout
+
     while True:
         m = list(itertools.islice(input, 0, BATCH_SIZE))
         if len(m) == 0:
@@ -33,7 +52,10 @@ def main():
             loss='hinge'
         )
         clf.fit(X, Y)
-        np.savetxt(sys.stdout, clf.coef_)
+        np.savetxt(output, clf.coef_)
+
+    if pycharm_mode:
+        input_f.close()
 
 if __name__ == '__main__':
     main()
--- project2/reducer.py
+++ project2/reducer.py
@@ -4,6 +4,25 @@
 import sys
 import numpy as np
 
-coef = np.loadtxt(sys.stdin)
-coef = np.mean(coef, axis=0)
-np.savetxt(sys.stdout, coef, newline=" ")
+pycharm_mode = False
+
+
+def process(input, output):
+    coef = np.loadtxt(input)
+    coef = np.mean(coef, axis=0)
+    np.savetxt(output, coef, newline=" ")
+
+if __name__ == "__main__":
+    if pycharm_mode:
+        import argparse
+        parser = argparse.ArgumentParser()
+        parser.add_argument("--input", help="The filename to be processed", required=True)
+        parser.add_argument("--output", help="The filename to be written to")
+        args = parser.parse_args()
+        if args.input:
+            with open(args.input) as f:
+                process(f, args.output if args.output else sys.stdout)
+                f.close()
+
+    else:
+        process(sys.stdin, sys.stdout)

e7da8b92a059759d2c403a4552c9f05b8873db7a
Jonathan Müller <j.mueller@apoveda.ch>
2015-11-05 13:58:54 +0100

    try combination

--- project2/mapper.py
+++ project2/mapper.py
@@ -6,17 +6,18 @@ import numpy as np
 import itertools
 from sklearn.svm import LinearSVC
 from sklearn.kernel_approximation import RBFSampler
+from sklearn.kernel_approximation import AdditiveChi2Sampler
 
-# make quickrun fails if this number is smaller than
-# the training_sub.txt line count
-BATCH_SIZE = 100
+BATCH_SIZE = 30000
 N_FEATURES = 400
 
-rbf_feature = RBFSampler(gamma=1, random_state=1, n_components=1000)
-rbf_feature.fit(np.zeros(N_FEATURES).ravel())
+chi = AdditiveChi2Sampler()
+chi.fit(np.zeros(N_FEATURES).ravel())
+rbf = RBFSampler(gamma=1, n_components=6000)
+rbf.fit(np.zeros(1200).ravel())
 
 def transform(x_original):
-    return rbf_feature.transform(x_original).ravel()
+    return rbf.transform(chi.transform(x_original)).ravel()
 
 def lines():
     for line in sys.stdin:

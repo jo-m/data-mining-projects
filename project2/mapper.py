67f689846d4d3a577180c6a27f15ffe2074e3349
Jonathan Müller <j.mueller@apoveda.ch>
2015-11-06 09:56:38 +0100

    dont use batch size anymore. just read everything from stdin we can get. this makes it much faster but breaks out test setup

--- project2/mapper.py
+++ project2/mapper.py
@@ -3,45 +3,34 @@
 
 import sys
 import numpy as np
-import itertools
 from sklearn.svm import LinearSVC
 from sklearn.kernel_approximation import RBFSampler
 from sklearn.kernel_approximation import AdditiveChi2Sampler
 
-BATCH_SIZE = 30000
 N_FEATURES = 400
 
 chi = AdditiveChi2Sampler()
 chi.fit(np.zeros(N_FEATURES).ravel())
-rbf = RBFSampler(gamma=1, random_state=1337, n_components=4800)
-rbf.fit(np.zeros(1200).ravel())
+rbf = RBFSampler(gamma=1, random_state=1337, n_components=6500)
+rbf.fit(np.zeros(N_FEATURES * 3).ravel())
 
 def transform(x_original):
-    return rbf.transform(chi.transform(x_original)).ravel()
+    return rbf.transform(chi.transform(x_original))
 
-def lines():
-    for line in sys.stdin:
-        line = line.strip()
-        (label, x_string) = line.split(" ", 1)
-        label = int(label)
-        x_original = np.fromstring(x_string, sep=' ')
-        yield label, transform(x_original)
+def load_data():
+    data = np.loadtxt(sys.stdin)
+    Y = data[:, 0]
+    X = data[:, 1:]
+    return transform(X), Y
 
 def main():
-    input = lines()
-    while True:
-        m = list(itertools.islice(input, 0, BATCH_SIZE))
-        if len(m) == 0:
-            break
-        X = np.vstack([x[1] for x in m])
-        Y = np.array([x[0] for x in m]).T
-
-        clf = LinearSVC(
-            fit_intercept=False,
-            loss='hinge'
-        )
-        clf.fit(X, Y)
-        np.savetxt(sys.stdout, clf.coef_)
+    X, Y = load_data()
+    clf = LinearSVC(
+        fit_intercept=False,
+        loss='squared_hinge',
+    )
+    clf.fit(X, Y)
+    np.savetxt(sys.stdout, clf.coef_)
 
 if __name__ == '__main__':
     main()

a4cb7da5318ae6d517fd7925da512c1309ebc8e4
Jonathan Müller <j.mueller@apoveda.ch>
2015-11-29 23:03:25 +0100

    add grade

---
+++ project3/grade.py
@@ -0,0 +1,16 @@
+#!/usr/bin/env python
+
+score = 10.73896
+
+easy = 13.93828
+hard = 9.01981
+
+def scale(score):
+    if score < hard:
+        return 1
+    if score > easy:
+        return 0
+    return (1 - (score - hard) / (easy - hard)) * 0.5 + 0.5
+
+print scale(score)
+print scale(score) * 5 + 1

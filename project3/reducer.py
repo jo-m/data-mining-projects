dd3c19ad1f4c3bda247380d705c4d8bc2cce8cf4
Jonathan Müller <j.mueller@apoveda.ch>
2015-11-30 17:51:03 +0100

    sub 3762

--- project3/reducer.py
+++ project3/reducer.py
@@ -5,7 +5,7 @@ import numpy as np
 import sys
 from sklearn.cluster import KMeans
 
-k = KMeans(n_clusters=100, n_init=15, copy_x=False)
+k = KMeans(n_clusters=100, n_init=10, copy_x=False)
 X = np.loadtxt(sys.stdin)
 k.fit(X)
 np.savetxt(sys.stdout, k.cluster_centers_)

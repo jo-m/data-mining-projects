83e82f083eac6b5e716051ec29cec3b96ce3c52f
Jonathan Müller <j.mueller@apoveda.ch>
2015-11-30 11:07:16 +0100

    sub 3726

--- project3/mapper.py
+++ project3/mapper.py
@@ -5,7 +5,7 @@ import numpy as np
 import sys
 from sklearn.cluster import MiniBatchKMeans
 
-k = MiniBatchKMeans(n_clusters=700, batch_size=700)
+k = MiniBatchKMeans(n_clusters=750, batch_size=1000)
 X = np.loadtxt(sys.stdin)
 k.fit(X)
 np.savetxt(sys.stdout, k.cluster_centers_)

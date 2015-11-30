787d458d5efd4de35cb41fc2a413c16e21922ef8
Jonathan Müller <j.mueller@apoveda.ch>
2015-11-30 18:02:24 +0100

    sub 3767

--- project3/reducer.py
+++ project3/reducer.py
@@ -3,9 +3,9 @@
 
 import numpy as np
 import sys
-from sklearn.cluster import KMeans
+from sklearn.cluster import MiniBatchKMeans
 
-k = KMeans(n_clusters=100, n_init=10, copy_x=False)
+k = MiniBatchKMeans(n_clusters=100, batch_size=700)
 X = np.loadtxt(sys.stdin)
 k.fit(X)
 np.savetxt(sys.stdout, k.cluster_centers_)

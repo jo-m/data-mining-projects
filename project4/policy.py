2033a791809d0a7da0f79f8e6f2388b8fc771e65
Jonathan Müller <j.mueller@apoveda.ch>
2015-12-14 23:18:34 +0100

    optimized, sub 4561

--- project4/policy.py
+++ project4/policy.py
@@ -3,51 +3,59 @@ import random
 from collections import defaultdict
 
 """
-st: (M, b) tuple for each article as a dict (key : article id)
-al: parameter alpha
-last_recommended: set by reccomend so `update` knows what article/user state to update for
+ARTICLES: (M, b) tuple for each article as a dict (key : article id)
+ALPHA: parameter
+LAST_RECOMMENDED: set by reccomend so `update` knows what article/user state to update for
 """
-st = None
-al = 1
-last_recommended = None
 user_ndim = 6
+# tuples M, M_inv, b, w
+ARTICLES = defaultdict(lambda: (
+    np.eye(user_ndim),
+    np.eye(user_ndim),
+    np.zeros(user_ndim),
+    np.zeros(user_ndim),
+))
+LAST_RECOMMENDED = None
+LAST_USER = None
+LAST_USER_SQ = None
+ALPHA = 0.2
+MODIFIED_M = set()
+MODIFIED_B = set()
 
 def set_articles(articles_):
-    global st, al, last_recommended, user_ndim
-    # tuples M, b
-    st = defaultdict(lambda: (
-        np.eye(user_ndim),
-        np.zeros(user_ndim)
-    ))
+    pass
 
-"""
-y: reward (-1: ignore, 1: click, 0: noclick)
-"""
-def update(y):
-    global st, al, last_recommended, user_ndim
-    if y == -1:
+def update(reward):
+    global ARTICLES, LAST_RECOMMENDED, LAST_USER, MODIFIED_M, MODIFIED_B
+    if reward < 0:
         return
-    a, z = last_recommended
-    M, b = st[a]
-    # print 'update for id=', a, 'reward=', y
-    M = M + np.outer(z, z)
-    b = b + y * z
-    st[a] = (M, b)
+    M, _, b, _ = ARTICLES[LAST_RECOMMENDED]
+    M += np.outer(LAST_USER, LAST_USER)
+    M_inv = np.linalg.inv(M)
+    MODIFIED_M.add(LAST_RECOMMENDED)
+    if reward == 1:
+        b += LAST_USER
+        MODIFIED_B.add(LAST_RECOMMENDED)
+    w = M_inv.dot(b)
+    ARTICLES[LAST_RECOMMENDED] = (M, M_inv, b, w)
 
 """
-time: int, not sequential
 z: 1 * 6 vector, user feature
 articles_list: list of article ids to choose from, len ~20
 """
-def reccomend(time, z, articles_list):
-    global st, al, last_recommended, user_ndim
-    z = np.array(z)
-    ucb = np.zeros(len(articles_list))
-    for i, a in enumerate(articles_list):
-        M, b = st[a]
-        M_inv = np.linalg.inv(M)
-        w = M_inv.dot(b)
-        ucb[i] = np.dot(w, z) + al * np.sqrt(np.dot(z, np.dot(M_inv, z)))
-    last_recommended = (articles_list[np.argmax(ucb)], z)
-    return last_recommended[0]
+def reccomend(_, z, articles_list):
+    global ARTICLES, LAST_RECOMMENDED, LAST_USER, LAST_USER_NORM
+    LAST_USER, LAST_USER_NORM = np.array(z), np.linalg.norm(z)
+    LAST_RECOMMENDED = max(articles_list, key=ucb)
+    return LAST_RECOMMENDED
 
+def ucb(article_id):
+    global LAST_USER, LAST_USER_NORM, MODIFIED_M, MODIFIED_B
+    z = LAST_USER
+
+    if not article_id in MODIFIED_M:
+        return ALPHA * LAST_USER_NORM
+    _, M_inv, _, w = ARTICLES[article_id]
+    if not article_id in MODIFIED_B:
+        return ALPHA * np.sqrt(z.dot(M_inv.dot(z)))
+    return np.dot(w, z) + ALPHA * np.sqrt(z.dot(M_inv.dot(z)))

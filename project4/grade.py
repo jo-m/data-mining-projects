79fb59ee6606146227c39c258ee9fa19dc98f2f1
Jonathan Müller <j.mueller@apoveda.ch>
2015-12-16 11:13:14 +0100

    update score

--- project4/grade.py
+++ project4/grade.py
@@ -1,13 +1,13 @@
 #!/usr/bin/env python
 
-score = 0.065707
+score = 0.06641
 
 easy = 0.044115
 hard = 0.065825
 
 def scale(score):
     if score > hard:
-        return 6
+        return 1
     if score < easy:
         return 0
     return (1 - (hard - score) / (hard - easy)) * 0.5 + 0.5
